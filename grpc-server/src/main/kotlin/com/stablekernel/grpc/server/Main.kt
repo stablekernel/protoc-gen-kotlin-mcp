894a7a836139b92de6fd3586e02da045d3bc9c59
Jeremy Brown <Jeremy.Brown14@accesscfa.com>
2025-06-04 13:12:19 -0600

    Good Vibes Only!

---
+++ grpc-server/src/main/kotlin/com/stablekernel/grpc/server/Main.kt
@@ -0,0 +1,24 @@
+package com.stablekernel.grpc.server
+
+import io.grpc.protobuf.services.ProtoReflectionService
+import io.ktor.server.engine.embeddedServer
+
+fun main() {
+  embeddedServer(GRpc, configure = {
+    port = 9999
+    serverConfigurer = {
+      addService(VibeService())
+      addService(ProtoReflectionService.newInstance())
+    }
+  }) {
+  }.start(wait = true)
+}
+
+private fun isJUnitTest(): Boolean {
+  return try {
+    Class.forName("org.junit.jupiter.api.Test")
+    true
+  } catch (e: ClassNotFoundException) {
+    false
+  }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,16 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/krpc/grpc")
+        mavenLocal()
+        google()
+    }
+}
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
+include(
+    "grpc-server"
+)
 rootProject.name = "protoc-gen-kotlin-mcp"
